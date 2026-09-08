b6cb03987139fcb1cf461ba7fea77b0ca816add3
Elihuangsddf <241110001@upmh.edu.mx>
2026-09-07 19:52:42 -0600

    Actualizacion 07/09/26

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,82 @@ class ViveroApp extends StatelessWidget {
           backgroundColor: Colors.green.shade400,
         ),
         body: Center(
-          child: Text('Bienvenido a Vivero+',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.green.shade700,
-          ),
-          ),
+          child: Padding(padding: 
+          const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(20),
+                decoration:BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: const Icon(
+                  Icons.eco,
+                  size:64,
+                  color: Color.fromARGB(255, 6, 102, 9),
+                ),
+              ),
+              const SizedBox(height: 24),
+              const Text(
+                'Bienvenidos a Vivero+',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 32, 118, 35),
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'Cuida tus plantas, cultiva tu mente.',
+                style: TextStyle(
+                  fontSize:16,
+                  color:Colors.grey 
+                ),
+              ),
+              const SizedBox(height: 36),
+              Row(
+                children:[
+                  Expanded(child: _StatCard(
+                    icon: Icons.local_florist,
+                    label:'3 por agregar',
+                  ),),
+                  const SizedBox(width:12),
+                  Expanded(child: _StatCard(
+                    icon: Icons.water_drop,
+                    label:'3 por regar',
+                  ),)
+                ]
+              )
+            ],
+          ))
           ),
       ),
       );
+  }
+}
+
+class _StatCard extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  const _StatCard({required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      decoration: BoxDecoration(
+        color: Colors.green.shade100,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child:Column(
+        children: [
+          Icon(icon, color: Colors.green),
+          const SizedBox(height: 8),
+          Text(label, style: const TextStyle(fontSize:13)),
+        ],
+      )
+    );
   }
 }
