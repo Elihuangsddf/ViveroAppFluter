2254d9d25aad5794f202b9fdba4b91c5de166fe1
Elihuangsddf <241110001@upmh.edu.mx>
2026-09-18 19:09:26 -0600

    Actualizacion 18 de Septiembre de 2026

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 
-
-
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool esFavorito = false;
+  void alternativoFavorito() {
+    setState(() {
+      esFavorito = !esFavorito;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'vivero',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('vivero'),
+    return Scaffold( 
+      appBar: AppBar(
+          title: const Text('ViveroMax'),
           backgroundColor: Colors.green.shade400,
         ),
         body: Center(
@@ -20,44 +28,59 @@ class HomeScreen extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-                  child: const Icon(
-                    Icons.eco,
-                    size: 64,
-                    color: Color.fromARGB(255, 6, 102, 9),
-                  ),
+                Stack(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: const Icon(
+                        Icons.eco,
+                        size: 100,
+                        color: Color.fromARGB(255, 6, 102, 9),
+                      ),
+                    ),
+                  ],
                 ),
                 const SizedBox(height: 24),
                 const Text(
-                  'Bienvenidos a vivero+',
-                  style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 23, 162, 25),
-                  ),
+                  'Bienvenido a ViveroMax',
+                  style: TextStyle(fontSize: 26, 
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 21, 161, 25)),
                 ),
                 const SizedBox(height: 8),
                 const Text(
-                  'cuida tus plantas, cultiva tu mente ',
+                  'Cuida tus plantas, cultiva tu mente',
                   style: TextStyle(fontSize: 16, color: Colors.grey),
                   textAlign: TextAlign.center,
                 ),
+                const SizedBox(height: 24),
+
+                ElevatedButton.icon(
+                  onPressed: alternativoFavorito, 
+                  icon: Icon(
+                    esFavorito ? Icons.favorite : Icons.favorite_border,
+                    color: esFavorito ? Colors.red : null,
+                  ), 
+                  label: Text(
+                    esFavorito ? 'En favoritos' : 'Agregar a favoritos'
+                  ),
+                ),
+                const SizedBox(height: 24),
                 Row(
                   children: [
                     Expanded(
-                      child: _StatCard(
+                      child: StatCard(
                         icon: Icons.local_florist,
-                        label: '22 plantas',
-                      ),
+                        label: '12 Plantas',
+                      )
                     ),
                     const SizedBox(width: 12),
                     Expanded(
-                      child: _StatCard(
+                      child: StatCard(
                         icon: Icons.water_drop,
                         label: '3 por regar',
                       ),
@@ -65,39 +88,38 @@ class HomeScreen extends StatelessWidget {
                   ],
                 ),
               ],
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        );
   }
 }
 
-class _StatCard extends StatelessWidget {
+class StatCard extends StatelessWidget {
   final IconData icon;
   final String label;
 
-  const _StatCard({required this.icon, required this.label});
+  const StatCard({required this.icon, required this.label});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(Object context) {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 16),
       decoration: BoxDecoration(
         color: Colors.green.shade100,
-        borderRadius: BorderRadius.circular(32),
+        borderRadius: BorderRadius.circular(12),
       ),
       child: Column(
         children: [
-          Icon(icon, color: Colors.green),
+          Icon(icon, color: Color.fromARGB(255, 6, 102, 9)),
           const SizedBox(height: 8),
-          Text(label, 
-          style: TextStyle(
-            fontSize: 13, 
-            color: Colors.green,
-            fontWeight: FontWeight.w100)),
-        ],
-      ),
+          Text(label, style: TextStyle(
+            fontSize: 13,
+            color: Color.fromARGB(255, 6, 102, 9),
+            fontWeight: FontWeight.w300,
+          )),
+        ]
+      )
     );
   }
 }
